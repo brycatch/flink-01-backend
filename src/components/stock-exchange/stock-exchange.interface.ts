7c7b28b4f2527b13e996ccd6b14ddc8cb71bdd18
Bryan Alejandro Sanchez Ramirez <sarabryanale@outlook.com>
2021-01-16 17:00:00 -0600

    NEW: Interface and model field for stock-exchange component

---
+++ src/components/stock-exchange/stock-exchange.interface.ts
@@ -0,0 +1,16 @@
+export interface IMarketValue {
+  value: number;
+  date: Date,
+  created: Date
+}
+
+export interface IStockExchange {
+  _id: string;
+  name: string;
+  description: string;
+  symbol: string;
+  market_values: IMarketValue[],
+  created: Date,
+  modified: Date,
+  active: boolean
+}
---
+++ src/components/stock-exchange/stock-exchange.model.ts
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+import unique from "mongoose-unique-validator";
+
+const schema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: [true, "ID required"],
+    unique: true
+  },
+  name: {
+    type: String,
+    required: [true, "Name required"],
+  },
+  description: {
+    type: String,
+    required: [true, "Description required"],
+  },
+  symbol: {
+    type: String,
+    required: [true, "Symbol required"],
+    unique: true
+  },
+  market_values: [{
+    _id: false,
+    value: {
+      type: Number,
+      required: [true, "Market value required"]
+    },
+    date: {
+      type: Date,
+      required: [true, "Market date required"]
+    },
+    created: {
+      type: Date,
+      default: new Date()
+    },
+  }],
+  created: {
+    type: Date,
+    default: new Date()
+  },
+  modified: {
+    type: Date,
+    default: new Date()
+  },
+  active: {
+    type: Boolean,
+    default: true
+  }
+}, {
+  versionKey: false
+});
+
+schema.plugin(unique, { message: '{PATH} should be unique value' });
+export default mongoose.model("StockExchange", schema);
